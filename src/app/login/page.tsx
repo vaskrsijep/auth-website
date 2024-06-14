c8a2a5062f24e39602a4ecee41248100b575a36e
kikij <panicvaskrsije@gmail.com>
2024-06-14 15:10:16 +0200

    adding components and pages for login and register

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,69 +1,56 @@
+import Nav from "@/components/nav";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
+        <Nav />
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
             href="/"
             rel="noopener noreferrer"
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
+            By vaske
           </a>
         </div>
       </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <a className="md:text-7xl text-5xl" href="">Login page</a>
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-2 lg:text-left">
-
-        <a
-          href="/login"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Login{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Login to your account
-          </p>
-        </a>
-
-        <a
-          href="/register"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Register{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Register an account
-          </p>
-        </a>
+      <div className="mt-32 flex items-center justify-center flex-col gap-10">
+        <h1 className="md:text-5xl text-5xl">Login page</h1>
+        <div className="h-1 border-t w-full"></div>
+        <form className=" flex items-center justify-center gap-7 flex-col w-full">
+          <div className="flex items-start justify-start gap-3 flex-col">
+            <label htmlFor="email">Email</label>
+            <input
+              type="text"
+              className="group rounded-lg border border-transparent px-5 py-4 transition-colors border-gray-300 bg-gray-100 dark:border-neutral-700 dark:bg-neutral-800/30"
+              placeholder="Email"
+              name="email"
+              id="email"
+            />
+          </div>
+          <div className="flex items-start justify-start gap-3 flex-col">
+            <label htmlFor="email">Password</label>
+            <input
+              type="password"
+              className="group rounded-lg border border-transparent px-5 py-4 transition-colors border-gray-300 bg-gray-100 dark:border-neutral-700 dark:bg-neutral-800/30"
+              placeholder="Password"
+              name="password"
+              id="password"
+            />
+          </div>
+          <Button variant="default">Login</Button>
+        </form>
+        <div className="flex flex-row w-full items-center justify-center">
+          <div className="h-1 border-t w-full" />
+          <div className="w-full flex items-center justify-center text-center font-medium text-lg">
+            or
+          </div>
+          <div className="h-1 border-t w-full" />
+        </div>
       </div>
     </main>
   );
---
+++ src/lib/auth.ts
@@ -0,0 +1,10 @@
+
+import NextAuth from "next-auth"
+import { CredentialsSignin } from "next-auth"
+import GitHub from "next-auth/providers/github"
+export const { handlers, auth } = NextAuth(req => {
+ if (req) {
+  console.log(req) // do something with the request
+ }
+ return { providers: [ GitHub ] }
+})
